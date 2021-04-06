94caa28e68265583094bc4146d4d71b2d3529f2e
reshetnikd <dmitry.reshetnik@icloud.com>
2021-04-06 11:39:55 +0300

    Add modes

--- ElementQuiz/ViewController.swift
+++ ElementQuiz/ViewController.swift
@@ -7,15 +7,30 @@
 
 import UIKit
 
+enum Mode {
+    case flashCard
+    case quiz
+}
+
+enum State {
+    case question
+    case answer
+}
+
 class ViewController: UIViewController {
     let elementList = ["Carbon", "Gold", "Chlorine", "Sodium"]
     var currentElementIndex = 0
+    var mode: Mode = .flashCard
+    var state: State = .question
     
     @IBOutlet weak var imageView: UIImageView!
     @IBOutlet weak var answerLabel: UILabel!
+    @IBOutlet weak var modeSelector: UISegmentedControl!
+    @IBOutlet weak var textField: UITextField!
     
     @IBAction func showAnswer(_ sender: UIButton) {
-        answerLabel.text = elementList[currentElementIndex]
+        state = .answer
+        updateUI()
     }
     
     @IBAction func next(_ sender: UIButton) {
@@ -24,21 +39,43 @@ class ViewController: UIViewController {
         if currentElementIndex >= elementList.count {
             currentElementIndex = 0
         }
-        updateElement()
+        state = .question
+        updateUI()
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        updateElement()
+        updateUI()
     }
 
-    func updateElement() {
+    // Updates the app's UI in flash card mode.
+    func updateFlachCardUI() {
         let elementName = elementList[currentElementIndex]
         let image = UIImage(named: elementName)
         
         imageView.image = image
-        answerLabel.text = "?"
+        
+        if state == .answer {
+            answerLabel.text = elementName
+        } else {
+            answerLabel.text = "?"
+        }
+    }
+    
+    // Updates the app's UI in quiz mode.
+    func updateQuizUI() {
+        
+    }
+    
+    // Updates the app's UI based on its mode and state.
+    func updateUI() {
+        switch mode {
+            case .flashCard:
+                updateFlachCardUI()
+            case .quiz:
+                updateQuizUI()
+        }
     }
 }
 
