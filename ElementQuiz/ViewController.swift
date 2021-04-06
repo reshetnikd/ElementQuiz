6321edca7847f0b1ea638152bb84aafdab7bcc08
reshetnikd <dmitry.reshetnik@icloud.com>
2021-04-06 11:01:48 +0300

    Add data

--- ElementQuiz/ViewController.swift
+++ ElementQuiz/ViewController.swift
@@ -8,12 +8,24 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    let elementList = ["Carbon", "Gold", "Chlorine", "Sodium"]
+    var currentElementIndex = 0
+    
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var answerLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        updateElement()
     }
 
-
+    func updateElement() {
+        let elementName = elementList[currentElementIndex]
+        let image = UIImage(named: elementName)
+        
+        imageView.image = image
+        answerLabel.text = "?"
+    }
 }
 
